35045858bb402cffdf8f878377fee85444be4b5c
Mason <davismasont@gmail.com>
2022-11-04 17:24:48 -0700

    add modules and paths for using models for all categories

---
+++ models/Comment.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {
+
+}
+
+Comments.init({
+    id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+        model: 'user',
+        key: 'id',
+      },
+  },
+  post_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+        model: 'post',
+        key: 'id',
+      },
+  }
+},{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+})
+
+module.exports = Comment;
---
+++ seeds/index.js
@@ -0,0 +1,7 @@
+
+
+const seedAll = async () => {
+
+
+}
+seedAll()
